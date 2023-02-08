25c89f4ec4e1ab15309922c5639d97b8d379c496
Claudio Galvão <claudiogaalvao@gmail.com>
2023-02-08 12:11:45 -0300

    Import theme and create search composable

--- app/src/main/java/com/example/codelabcompose/MainActivity.kt
+++ app/src/main/java/com/example/codelabcompose/MainActivity.kt
@@ -4,40 +4,65 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.codelabcompose.ui.theme.CodelabComposeTheme
+import androidx.compose.ui.unit.dp
+import com.example.codelabcompose.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CodelabComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MySootheTheme {
+                HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        SearchBar()
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = { Text(text = stringResource(id = R.string.placeholder_search)) },
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    )
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    SearchBar()
+}
+
+@Preview(showBackground = true, heightDp = 320, widthDp = 200)
 @Composable
 fun DefaultPreview() {
-    CodelabComposeTheme {
-        Greeting("Android")
+    MySootheTheme {
+        HomeScreen()
     }
 }
